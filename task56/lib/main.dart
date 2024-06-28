e956b4741b94ed370c2de5b1a1f3eb238882a37a
AmbroCust <102913958+AmbroCust@users.noreply.github.com>
2024-06-29 02:59:09 +0700

    New Task Uploaded Task56

---
+++ task56/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LottieScreen(),
+    );
+  }
+}
+
+class LottieScreen extends StatelessWidget {
+  const LottieScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Анекдоты онлайнъ'),
+      ),
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/lottie_animation.json',  // Путь к вашему файлу Lottie
+          width: 500,
+          height: 500,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
